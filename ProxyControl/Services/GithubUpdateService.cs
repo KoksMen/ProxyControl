869541a241adcbd8c61d06738defdb216cc32211
rinsh <rinsh@RINSHIMALAPTOP>
2026-01-29 10:19:58 +0300

    Change version

--- ProxyControl/Services/GithubUpdateService.cs
+++ ProxyControl/Services/GithubUpdateService.cs
@@ -22,7 +22,7 @@ namespace ProxyControl.Services
             {
                 using (var client = new HttpClient())
                 {
-                    client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("ProxyControl", "1.9.1"));
+                    client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("ProxyControl", "1.9.7"));
 
                     string url = $"https://api.github.com/repos/{RepoOwner}/{RepoName}/releases/latest";
                     var response = await client.GetStringAsync(url);
